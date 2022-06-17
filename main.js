37be964fa23dca2d68bfa7b46031fbb7bf27cb0a
Yeyzon Gonzalez <yeyzon75@gmail.com>
2022-06-17 16:54:56 -0600

    Se muestran los numeros al presionar botones

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-/* Comenzamos con Javascript */
+/* Limpiar Pantalla */
+let numbers = document.querySelector(".numbers");
+const btnDeleteDisplay = document.querySelector(".deleteDisplay");
+btnDeleteDisplay.addEventListener("click", () => {
+  numbers.value = "";
+});
+
+/* Recorrer botones y el punto*/
+document.querySelectorAll(".button").forEach((e) => {
+  e.addEventListener("click", showNumber, false);
+});
+
+/* Mostrar Numeros Pulsados En Pantalla */
+function showNumber() {
+  let btnValue = this.value;
+  numbers.value += btnValue;
+}
